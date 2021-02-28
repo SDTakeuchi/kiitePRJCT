4ef439fa11494662b8fce6b60782ed54d2c3fe87
SDTakeuchi <kuranku191952996@gmail.com>
2021-02-28 10:57:30 +0900

    layout fixed

--- km/filters.py
+++ km/filters.py
@@ -1,6 +1,15 @@
 import django_filters
-from django_filters import CharFilter
+from django_filters import CharFilter, conf
 from .models import *
+
+def FILTERS_VERBOSE_LOOKUPS():
+    from django_filters.conf import DEFAULTS
+
+    verbose_lookups = DEFAULTS['VERBOSE_LOOKUPS'].copy()
+    verbose_lookups.update({
+        'icontains':'',
+    })
+    return verbose_lookups
 
 class PostFilter(django_filters.FilterSet):
     title = CharFilter(field_name='title', lookup_expr='icontains', label='質問タイトル')
