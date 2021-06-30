108987c32dc583125ad1d8e1e137d1974b36d957
SDTakeuchi <kuranku191952996@gmail.com>
2021-06-30 16:48:40 +0900

    line notifi func added

--- km/cron.py
+++ km/cron.py
@@ -4,9 +4,9 @@ from django.template.loader import render_to_string
 from datetime import datetime
 from km.models import *
 import km.views
+from km.line_notifi import send_msg_to_line
 
 def dailymail():
-
     today1959 = datetime.datetime.now().replace(hour=19,minute=59,second=45, microsecond=0)
     yesterday2000 = today1959 - datetime.timedelta(days=1)
     yesterday2000 = yesterday2000.replace(hour=20,minute=0,second=0, microsecond=0)
@@ -43,7 +43,24 @@ def dailymail():
 
         message += render_to_string('email_template/dailymail/lower_message.txt')
         # recepient = list(toUser) #in actual page, the mail is delivered to all alumni
-        recepient = ["123@gmail.com"] #erace this line for actual environment
+        recepient = ["kuranku191952996@gmail.com"] #erace this line for actual environment
         
         msg = EmailMessage(subject, message, EMAIL_HOST_USER, bcc=recepient)
         msg.send()
+
+        #below for LINE
+        msg_for_line = "【Kiite-me!】に本日届いた質問をお送りします！ご確認お願いします！\n"
+        for question in posts:
+            msg_for_line += "---------------------------------------"
+            msg_for_line += '\n'
+            msg_for_line += question.user.name 
+            msg_for_line += '\n'
+            msg_for_line += truncate(question.title, 23)
+            msg_for_line += '\n\n'
+            msg_for_line += truncate(question.body, 90)
+            msg_for_line += '\n'
+            msg_for_line += 'https://kiite-me.site/posts/show/' + str(question.id)
+            msg_for_line += '\n'
+            msg_for_line += '\n'
+        msg_for_line += "---------------------------------------"
+        send_msg_to_line(msg_for_line)
---
+++ km/line_notifi.py
@@ -0,0 +1,7 @@
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+from kiite_me.settings import LINE_CHANNEL_ACCESS_TOKEN
+
+def send_msg_to_line(msg):
+    line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+    line_bot_api.broadcast(TextSendMessage(text=msg))
